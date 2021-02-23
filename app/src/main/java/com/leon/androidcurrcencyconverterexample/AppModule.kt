413162d3e9e7fb469b19d40eec87fdebc61dc25f
leonkamerlinn <leonkamerlin@MacBook-Pro.local>
2021-02-24 00:46:37 +0100

    added dependecies

---
+++ app/src/main/java/com/leon/androidcurrcencyconverterexample/AppModule.kt
@@ -0,0 +1,20 @@
+package com.leon.androidcurrcencyconverterexample
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    @Named("String1")
+    fun provideTestString1() = "This is a string we will inject"
+
+}
